f42a45ddafc0c459891e70d33ca9a9c58ea572fd
PedroBastosBS <pedro.silva@attointelligence.com.br>
2023-11-10 22:34:58 -0400

    chore: add doc e gerador de array

---
+++ estrutura_dados/Terceira_Lista/alg_ordenacao_node/utils/gerador_array.js
@@ -0,0 +1,12 @@
+const random = require('random');
+
+function generateRandomArray(size) {
+  const array = Array.from({ length: size }, () => random.int(1, size + 1));
+  return array;
+}
+
+export function generateArrays(sizes) {
+  return sizes.map(size => generateRandomArray(size));
+}
+
+
